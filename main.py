ac2a2573b80fd507f7f94c05c33caed52bccae25
vidagharavian <vgharavian@gmail.com>
2023-04-05 21:37:34 -0400

    dataset updated

---
+++ main.py
@@ -0,0 +1,52 @@
+import torch
+from dgl import load_graphs
+
+from model import DiGCN_Inception_Block_Ranking, SAGE
+import torch.nn.functional as F
+graph, label_dict = load_graphs("./data.bin", [0])
+graph=graph[0]
+node_features = graph.ndata['feat']
+node_labels = graph.ndata['label'].type(torch.LongTensor)
+train_mask = graph.ndata['train_mask']
+valid_mask = graph.ndata['val_mask']
+test_mask = graph.ndata['test_mask']
+n_features = node_features.shape[1]
+n_labels = int(node_labels.max().item() + 1)
+
+def evaluate(model, graph, features, labels, mask,method_name):
+    model.eval()
+    with torch.no_grad():
+        if method_name == 'SAGE':
+            logits = model(graph, features)
+        else:
+            logits = model(graph.edges(),(graph.edata['weight'],graph.edata['weight']),features)
+        logits = logits[mask]
+        labels = labels[mask]
+        _, indices = torch.max(logits, dim=1)
+        correct = torch.sum(indices == labels)
+        return correct.item() * 1.0 / len(labels)
+
+method_name ='DiGCN_Inception_Block_Ranking'
+if method_name == 'SAGE':
+    model = SAGE(in_feats=n_features, hid_feats=100, out_feats=n_labels)
+else:
+    model = DiGCN_Inception_Block_Ranking(num_features=n_features,embedding_dim=32,out=n_labels,dropout=0.2)
+opt = torch.optim.Adam(model.parameters())
+
+for epoch in range(100):
+    model.train()
+    # forward propagation by using all nodes
+    if method_name =='SAGE':
+        logits = model(graph,node_features)
+    else:
+        logits = model(graph.edges(),(graph.edata['weight'],graph.edata['weight']),node_features)
+    # compute loss
+    loss = F.cross_entropy(logits[train_mask], node_labels[train_mask])
+    # compute validation accuracy
+    acc = evaluate(model, graph, node_features, node_labels, test_mask,method_name)
+    print("accuracy: "+str(acc))
+    # backward propagation
+    opt.zero_grad()
+    loss.backward()
+    opt.step()
+    print("loss: "+str(loss.item()))
--- model.py
+++ model.py
@@ -1,51 +1,85 @@
 import torch
+from typing import Tuple
 
+from torch import nn
 
+from DiGCN_Inception_Block import DiGCN_InceptionBlock as InceptionBlock
+import torch.nn.functional as F
+import dgl.nn as dglnn
 
-class DIMPA(torch.nn.Module):
-    r"""The directed mixed-path aggregation model.
-
+class DiGCN_Inception_Block_Ranking(nn.Module):
+    r"""The ranking model adapted from the
+    `Digraph Inception Convolutional Networks"
+    <https://papers.nips.cc/paper/2020/file/cffb6e2288a630c2a787a64ccc67097c-Paper.pdf>`_ paper.
     Args:
-        hop (int): Number of hops to consider.
+        * **num_features** (int): Number of features.
+        * **dropout** (float): Dropout probability.
+        * **embedding_dim** (int) - Embedding dimension.
+        * **Fiedler_layer_num** (int, optional) - The number of single Filder calculation layers, default 3.
+        * **alpha** (float, optional) - (Initial) learning rate for the Fiedler step, default 0.01.
+        * **trainable_alpha** (bool, optional) - Whether alpha is trainable, default False.
+        * **initial_score** (torch.FloatTensor, optional) - Initial guess of scores, default None.
+        * **sigma** (float, optionial) - (Initial) Sigma in the Gaussian kernel, actual sigma is this times sqrt(num_nodes), default 1.
+        * **kwargs (optional): Additional arguments of
+            :class:`RankingGNNBase`.
     """
 
-    def __init__(self, hop: int):
-        super(DIMPA, self).__init__()
-        self._hop = hop
-        self._w_s = Parameter(torch.FloatTensor(hop + 1, 1))
-        self._w_t = Parameter(torch.FloatTensor(hop + 1, 1))
+    def __init__(self, num_features: int, dropout: float, embedding_dim: int,out:int):
+        super().__init__()
+        self.ib1 = InceptionBlock(num_features, embedding_dim)
+        self.ib2 = InceptionBlock(embedding_dim, out)
+        self._dropout = dropout
+        self.reset_parameters()
 
-        self._reset_parameters()
+    def reset_parameters(self):
+        self.ib1.reset_parameters()
+        self.ib2.reset_parameters()
 
-    def _reset_parameters(self):
-        self._w_s.data.fill_(1.0)
-        self._w_t.data.fill_(1.0)
-
-    def forward(self, x_s: torch.FloatTensor, x_t: torch.FloatTensor,
-                A: Union[torch.FloatTensor, torch.sparse_coo_tensor],
-                At: Union[torch.FloatTensor, torch.sparse_coo_tensor]) -> Tuple[torch.FloatTensor, torch.FloatTensor, torch.LongTensor, torch.FloatTensor]:
+    def forward(self,edge_index_tuple: Tuple[torch.LongTensor, torch.LongTensor],
+                edge_weight_tuple: Tuple[torch.FloatTensor, torch.FloatTensor],
+                features: torch.FloatTensor, ) -> torch.FloatTensor:
         """
-        Making a forward pass of DIMPA.
-
+        Making a forward pass.
         Arg types:
-            * **x_s** (PyTorch FloatTensor) - Source hidden representations.
-            * **x_t** (PyTorch FloatTensor) - Target hidden representations.
-            * **A** (PyTorch FloatTensor or PyTorch sparse_coo_tensor) - Row-normalized adjacency matrix.
-            * **At** (PyTorch FloatTensor or PyTorch sparse_coo_tensor) - Tranpose of column-normalized adjacency matrix.
+            * edge_index_tuple (PyTorch LongTensor) - Tuple of edge indices.
+            * edge_weight_tuple (PyTorch FloatTensor, optional) - Tuple of edge weights corresponding to edge indices.
+            * features (PyTorch FloatTensor) - Node features.
 
         Return types:
-            * **feat** (PyTorch FloatTensor) - Embedding matrix, with shape (num_nodes, 2*input_dim).
+            * z (PyTorch FloatTensor) - Embedding matrix.
         """
-        feat_s = self._w_s[0]*x_s
-        feat_t = self._w_t[0]*x_t
-        curr_s = x_s.clone()
-        curr_t = x_t.clone()
-        for h in range(1, 1+self._hop):
-            curr_s = torch.matmul(A, curr_s)
-            curr_t = torch.matmul(At, curr_t)
-            feat_s += self._w_s[h]*curr_s
-            feat_t += self._w_t[h]*curr_t
+        x = features
+        edge_index, edge_index2 = edge_index_tuple
+        edge_index, edge_index2 = edge_index.reshape(1,edge_index.size(0)), edge_index.reshape(1,edge_index2.size(0))
+        edge_index, edge_index2 = torch.cat((edge_index,edge_index2), 0),torch.cat((edge_index,edge_index2), 0)
+        edge_weight, edge_weight2 = edge_weight_tuple
+        x0, x1, x2 = self.ib1(x, edge_index, edge_weight, edge_index2, edge_weight2)
+        x0 = F.dropout(x0, p=self._dropout)
+        x1 = F.dropout(x1, p=self._dropout)
+        x2 = F.dropout(x2, p=self._dropout)
+        x = x0.type(torch.DoubleTensor) + x1.type(torch.DoubleTensor) + x2.type(torch.DoubleTensor)
+        x = F.dropout(x, p=self._dropout)
 
-        feat = torch.cat([feat_s, feat_t], dim=1)  # concatenate results
+        x0, x1, x2 = self.ib2(x, edge_index, edge_weight, edge_index2, edge_weight2)
+        x0 = F.dropout(x0, p=self._dropout)
+        x1 = F.dropout(x1, p=self._dropout)
+        x2 = F.dropout(x2, p=self._dropout)
+        x = x0 + x1.type(torch.DoubleTensor) + x2.type(torch.DoubleTensor)
+        self.z = x.clone()
 
-        return feat
+        return self.z
+
+class SAGE(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats):
+        super().__init__()
+        self.conv1 = dglnn.SAGEConv(
+            in_feats=in_feats, out_feats=hid_feats, aggregator_type='mean')
+        self.conv2 = dglnn.SAGEConv(
+            in_feats=hid_feats, out_feats=out_feats, aggregator_type='mean')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = F.relu(h)
+        h = self.conv2(graph, h)
+        return h
